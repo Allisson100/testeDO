d11df259dae1c27dc1221b6957394c8438cfb140
Allisson100 <allisson.boiane@outlook.com>
2024-07-07 22:00:02 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const https = require("https");
+const fs = require("fs");
+const express = require("express");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3031;
+const app = express();
+
+const httpsSever = https.createServer(
+  {
+    key: fs.readFileSync("keys/127.0.0.1-key.pem", "utf8"),
+    cert: fs.readFileSync("keys/127.0.0.1.pem", "utf8"),
+  },
+  app
+);
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API ..." });
+});
+
+httpsSever.listen(PORT, () => {
+  console.log(`Server running ... PORT: ${PORT}`);
+});
